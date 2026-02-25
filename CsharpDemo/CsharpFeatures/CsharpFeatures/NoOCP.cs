89fc2dab1e6f2359e7799770ef793cded44ed87a
Ponnims <Ponnieaswari@ftpoc.trumio.ai>
2026-02-25 11:47:34 +0530

    upd

---
+++ CsharpDemo/CsharpFeatures/CsharpFeatures/NoOCP.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpFeatures
+{
+    class Notification
+    {
+        public string Message { get; set; }
+        public string Notifythrough { get; set; }
+        public void Notify(string message, string notifyThrough)
+        {
+            Message = message;
+            if (notifyThrough == "Email")
+            {
+                Notifythrough = "Email";
+                Console.WriteLine("Notification sent through Email: " + Message);
+            }
+            else if (notifyThrough == "SMS")
+            {
+                Notifythrough = "SMS";
+                Console.WriteLine("Notification sent through SMS: " + Message);
+            }
+            else if (notifyThrough == "Whatsapp")
+            {
+                Notifythrough = "Whatsapp";
+                Console.WriteLine("Notification sent through Push: " + Message);
+            }
+            else if(notifyThrough== "Insta")
+            {                 Notifythrough = "Insta";
+                Console.WriteLine("Notification sent through Insta: " + Message);
+            }
+        }
+    }
+    internal class NoOCP
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ CsharpDemo/CsharpFeatures/CsharpFeatures/OCPdemo.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpFeatures
+{
+    interface INotification
+    {
+        void Notify(string message);
+    }
+
+    class EmailNotification : INotification
+    {
+        public void Notify(string message)
+        {
+            Console.WriteLine($"Email notification: {message}");
+        }
+    }
+
+    class SMSNotification : INotification
+    {
+        public void Notify(string message)
+        {
+            Console.WriteLine($"SMS notification: {message}");
+        }
+    }
+
+    class WhatsAppNotification : INotification
+    {
+        public void Notify(string message)
+        {
+            Console.WriteLine($"WhatsApp notification: {message}");
+        }
+    }
+
+    
+
+    class InstagramNotification : INotification
+    {
+        public void Notify(string message)
+        {
+            Console.WriteLine($"Instagram notification: {message}");
+        }
+    }
+
+    class TwitterNotification : INotification
+    {
+        public void Notify(string message)
+        {
+            Console.WriteLine($"Twitter notification: {message}");
+        }
+    }
+
+    internal class OCPdemo
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
