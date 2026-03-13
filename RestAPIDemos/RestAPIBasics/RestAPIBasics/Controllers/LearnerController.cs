b127e71cf59e1e3dadb76c62598f291541da747e
Ponnims <Ponnieaswari@ftpoc.trumio.ai>
2026-03-13 11:35:05 +0530

    upd

---
+++ RestAPIDemos/RestAPIBasics/RestAPIBasics/Controllers/LearnerController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestAPIBasics.Models;
+
+namespace RestAPIBasics.Controllers
+{
+    [Route("api/ust/[controller]")]
+    [ApiController]
+    public class LearnerController : ControllerBase
+    {
+        [HttpGet]
+        [Route("getlearnerss")]
+        public IActionResult GetLearners()
+        {
+            var learners = new List<string> { "Alice", "Bob", "Charlie" };
+            return Ok(learners);
+        }
+
+        [HttpGet("anotherget")]
+
+        public IActionResult AnotherGet()
+        {
+            return Ok("This is another GET endpoint.");
+        }
+
+        [HttpGet("getlearnerbyid/{id}")]
+        public IActionResult GetLearnerById(int id)
+        {
+            var learner = $"Learner {id}";
+            return Ok(learner);
+        }
+
+        [HttpGet("methodwithquerystr")]
+        public IActionResult methodwithquerystr([FromQuery]string input)
+        {
+            // example: api/ust/learner/methodwithquerystr?input=hello
+            return Ok($"You entered: {input}");
+        }
+
+        [HttpGet("methodwithquerystrlearnerdata")]
+        public IActionResult methodwithquerystrl([FromBody] Learner data)
+        {
+            // example: api/ust/learner/methodwithquerystr?input=hello
+            return Ok($"You entered: Id: {data.Id},Name:  {data.Name}, Stream: {data.Stream}, Score: {data.Score}");
+        }
+
+        [HttpPost("postlearnerdata")]
+        public ActionResult<Learner> postlearnerdata([FromBody] Learner data)
+        {
+            // example: api/ust/learner/methodwithquerystr?input=hello
+            return Ok($"You entered: Id: {data.Id},Name:  {data.Name}, Stream: {data.Stream}, Score: {data.Score}");
+        }
+    }
+}
---
+++ RestAPIDemos/RestAPIBasics/RestAPIBasics/Models/Learner.cs
@@ -0,0 +1,10 @@
+﻿namespace RestAPIBasics.Models
+{
+    public class Learner
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Stream { get; set; }
+        public int Score { get; set; }
+    }
+}
