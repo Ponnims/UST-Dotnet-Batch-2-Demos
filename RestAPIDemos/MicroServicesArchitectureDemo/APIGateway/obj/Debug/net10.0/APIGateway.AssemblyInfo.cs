30a9bf7d57d76e847d26b4ac84d88f8e65b0f50c
Ponnims <Ponnieaswari@ftpoc.trumio.ai>
2026-03-23 11:48:59 +0530

    upd

--- RestAPIDemos/MicroServicesArchitectureDemo/APIGateway/obj/Debug/net10.0/APIGateway.AssemblyInfo.cs
+++ RestAPIDemos/MicroServicesArchitectureDemo/APIGateway/obj/Debug/net10.0/APIGateway.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APIGateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+738ab54f3fa9433cc0e5c7841f200adb30cb6ad4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e2d9c4cec6977b34ffd4d3e312013dc4621bde1")]
 [assembly: System.Reflection.AssemblyProductAttribute("APIGateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APIGateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
